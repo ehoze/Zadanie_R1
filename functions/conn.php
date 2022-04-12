2ea268c80bc6f939a01b4572ce7be8c657197f17
ehoze <eryk_kucharski@tlen.pl>
2022-04-12 23:42:09 +0200

    pliki

--- functions/conn.php
+++ functions/conn.php
@@ -17,7 +17,7 @@ class Database
     {
 
         try {
-            $conn = new PDO("mysql:host=db4free.net;dbname=eryk_kucharski", $username, $password);
+            $conn = new PDO("mysql:host=db4free.net;dbname=eryk_kucharski", "eryk_kucharski", "eryk_kucharskibazadanych");
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
