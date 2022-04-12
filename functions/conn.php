c423c65af53f2674b70a565c4f13bb454cf49f72
ehoze <eryk_kucharski@tlen.pl>
2022-04-12 22:25:39 +0200

    pliki

--- functions/conn.php
+++ functions/conn.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "db4free";
+$servername = "db4free.net";
 $db = "eryk_kucharski";
 $username = "eryk_kucharski";
 $password = "eryk_kucharskibazadanych";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
